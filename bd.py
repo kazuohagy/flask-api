4d858f8a64f794a3a0aa3fe8327db6220fcce644
kazuo <kazuo.hagy@gmail.com>
2023-03-02 14:26:31 -0300

    Commit Lucca

--- bd.py
+++ bd.py
@@ -1,5 +1,5 @@
 Nomes = [
-    {'id': 1, 
+    {'id': 1,
      'name': 'LuccaGay',
      'email': 'lucca@gmail.com',
      'senha': '123'},
@@ -8,4 +8,10 @@ Nomes = [
      'email': 'kazuo@gmail.com',
      'senha': '123'
      }
-         ]
+]
+# Lucca se voce estiver lendo isso, eu te amo, e eu sabia que voce ia ler isso.
+# Obs a Angelica e a Lucca sao minhas namoradas, e eu amo as duas.
+# Obs2: Eu amo a Lucca mais que a Angelica, mas eu amo as duas.
+# Isso foi um teste, eu nao sou gay, eu so queria ver se voce ia ler isso.
+# Eu amo a Lucca, e eu amo a Angelica, e eu amo as duas.
+# Tamo junto meu parceiro bom trabalho
